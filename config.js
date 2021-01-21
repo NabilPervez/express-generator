22a8d23aefb96d00baeb4eeb968cbbfd1e0207d3
Nabil Pervez <perveznabil@gmail.com>
2021-01-20 20:33:55 -0600

    passport fb

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '376016283571811',
+        clientSecret: '6f18d99df0a3aa6285c6b785bb2bc603'
+    }
 }
